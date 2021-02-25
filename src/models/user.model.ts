855d75e59331be496c4141c8f77187e752ce008b
cgraaaj <cgrvel@gemail.com>
2021-02-25 20:58:54 +0530

    1stcommit

---
+++ src/models/user.model.ts
@@ -0,0 +1,41 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class User extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: false,
+    required: true,
+  })
+  id: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+  })
+  email?: string;
+
+  @property({
+    type: 'array',
+    itemType: 'object',
+    required: true,
+  })
+  books: object[];
+
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
+
+export interface UserRelations {
+  // describe navigational properties here
+}
+
+export type UserWithRelations = User & UserRelations;
